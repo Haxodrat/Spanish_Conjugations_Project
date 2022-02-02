1e780d682aa7283eef556e4ca80ddc00453207c7
Christopher Kim <haxodrat@icloud.com>
2022-02-01 16:17:01 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        //region Array Lists
+        //region Arrays
 
         //region Verb Roots
         String[] verbRoots = {"andar", "ir", "comer", "nadar", "dormir", "averiguar", "cantar",
